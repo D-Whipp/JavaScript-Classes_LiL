a45d70be0ac01b03beb123336c50a8992d28df1e
Mr Whipp <dwhipp88@gmail.com>
2022-08-08 02:05:48 -0600

    added updating list functionality

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,7 +1,7 @@
 // const Jasnah = new Person('Jasnah', 6, 156, 34);
 // console.log(Jasnah.personStats());
 // hoistableFunction();
-
+const radiantList = document.getElementById('knights-radiant-list');
 // mixin's are fround upon
 let mixin = {
   madeIn() {
@@ -18,15 +18,16 @@ let radiantMixin = {
 };
 
 class Radiant {
-  constructor(name, height, weight, age) {
+  constructor(name, heightFeet, heightInches, weight, age) {
     this.name = name;
-    this.height = height;
+    this.heightFt = heightFeet;
+    this.heightIn = heightInches;
     this.weight = weight;
     this.age = age;
   }
 
   radiantStats() {
-    return `${this.name} is ${this.height} ft tall, weighs ${this.weight} pounds, and is ${this.age} years old.`;
+    return `${this.name} is ${this.heightFt} feet ${this.heightIn} inches tall, weighs ${this.weight} pounds, and is ${this.age} years old.`;
   }
 
   static totalPowerLevel(power1, power2) {
@@ -38,8 +39,17 @@ class Radiant {
 }
 
 class Windrunners extends Radiant {
-  constructor(name, height, weight, age, radiantStats, power, spren) {
-    super(name, height, weight, age, radiantStats);
+  constructor(
+    name,
+    heightFeet,
+    heightInches,
+    weight,
+    age,
+    radiantStats,
+    power,
+    spren
+  ) {
+    super(name, heightFeet, heightInches, weight, age, radiantStats);
     this.power = power;
     this.spren = spren;
     this.team = 'Windrunner';
@@ -54,23 +64,62 @@ class Windrunners extends Radiant {
   }
 }
 
-const Kal = new Radiant('Kaladin', 6, 200, 23);
+const Kal = new Radiant('Kaladin', 6, 11, 250, 23);
 const Teft = new Windrunners(
   'Teft',
-  5.11,
-  175,
+  6,
+  7,
+  225,
   53,
   'Flight',
   'Phendorana'
 );
+
+// radiantList.push(Kal);
+// radiantList.push(Teft);
+// console.log(radiantList);
+
 // const Navani = new Radiant('Navani', 6, 155, 44);
-console.log(Teft);
-console.log(Teft.myName());
-console.log(Teft.madeIn());
-console.log(Kal);
+// console.log(Teft);
+// console.log(Teft.myName());
+// console.log(Teft.madeIn());
+// console.log(Kal);
 // console.log(Kal.radiantStats());
 // console.log(Radiant.totalPowerLevel(Kal, Navani));
 
 // function hoistableFunction() {
 //   return console.log('I can be called from anywhere!');
 // }
+
+// DOM manipulation
+function addRadiant() {
+  let ul = document.getElementById('knights-radiant-list');
+
+  let radiant = document.getElementById('radiantInput');
+  let li = document.createElement('li');
+  li.setAttribute('id', 'radiant');
+  li.appendChild(document.createTextNode(radiant.value));
+  ul.appendChild(li);
+}
+
+function removeRadiant() {
+  let ul = document.getElementById('knights-radiant-list');
+  ul.removeChild(ul.firstElementChild);
+  // let radiant = document.getElementById('radiantInput');
+  // let item = document.getElementById(radiant.value);
+  // ul.removeChild(item);
+}
+
+// const bodyNode = document.querySelector('body');
+// const ulNode = document.createElement('ul');
+// const liNode = document.createElement('li');
+// const liNodeTwo = document.createElement('li');
+// liNode.innerHTML = "I'm listening...";
+// liNode.innerHTML = Kal.radiantStats();
+// liNodeTwo.innerHTML = Teft.radiantStats();
+// radiantList.appendChild(liNode);
+// console.log(radiantList);
+// radiantList.appendChild(liNodeTwo);
+// const liUlNode = ulNode.appendChild(liNode);
+// const liUlNode = ulNode.appendChild(liNodeTwo);
+// bodyNode.appendChild(liUlNode);
